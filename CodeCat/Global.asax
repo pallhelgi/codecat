0f45af4074ad7e7d65c587b963e30f440080d234
Eyrun <eyrunh16@ru.is>
2017-04-29 16:24:40 +0000

    Created the ASP.NET project in Visual Studio 2015 Enterprise

---
+++ CodeCat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeCat.MvcApplication" Language="C#" %>
---
+++ CodeCat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeCat.Startup))]
+namespace CodeCat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
