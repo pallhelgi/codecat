cb68f4e09ff594ff5fc17160eeff1a3a373e4cca
Gudrun Vald <gudrunv16@ru.is>
2017-05-03 19:10:56 +0000

    Project Model moved to Models folder, data annotations added to the ProjectModel class

--- CodeCat/Models/ProjectModel.cs
+++ CodeCat/Models/ProjectModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace CodeCat.Models
     public class ProjectModel
     {
         public int ID { get; set; }
+        [Display(Name = "Project name")]
+        [Required(ErrorMessage = "You must enter a name for the project!")]
         public string name { get; set; }
         public int creatorUserID { get; set; }
     }
