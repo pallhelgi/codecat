a20f0793b65d63b8691e110ac8d88bf4ba5ac93a
eydisar <eydisar@gmail.com>
2017-05-03 11:21:17 +0000

    Build error fixed

--- CodeCat/Services/DocumentService.cs
+++ CodeCat/Services/DocumentService.cs
@@ -8,16 +8,5 @@ namespace CodeCat.Services
 {
     public class DocumentService
     {
-        public DocumentModel document;
-
-        public DocumentModel getDocument(int documentID)
-        {
-            return null;
-        }
-
-        public bool addDocument()
-        {
-            return false;
-        }
     }
 }
