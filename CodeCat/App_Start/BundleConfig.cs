e98595fb74ad5665ea7c5740ce63ea80896c263b
Gudrun Vald <gudrunv16@ru.is>
2017-05-03 14:43:48 +0000

    Created Front Page View (Home/Index.cshtml)

--- CodeCat/App_Start/BundleConfig.cs
+++ CodeCat/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace CodeCat
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/custom.css"));
         }
     }
 }
--- CodeCat/Services/DocumentService.cs
+++ CodeCat/Services/DocumentService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using CodeCat.Models;
-using CodeCat.Models;
 
 namespace CodeCat.Services
 {
