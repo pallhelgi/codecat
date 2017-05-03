0a28a15b25713dab96c3daf8a6f43e9d26667dda
eydisar <eydisar@gmail.com>
2017-05-03 10:57:25 +0000

    ProjectService basic functions added - no functionality yet

--- CodeCat/Services/ProjectService.cs
+++ CodeCat/Services/ProjectService.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using CodeCat.Models;
 
 namespace CodeCat.Services
 {
     public class ProjectService
     {
+        public ProjectModel project;
+
+        public bool getProject(int projectID)
+        {
+            return false;
+        }
+
+        public bool addProject(int creatorUserID, ProjectModel project)
+        {
+            return false;
+        }
+
+        public UserModel getProjectCreator(int projectID)
+        {
+            return null;
+        }
+
+        public bool share(int userID)
+        {
+            return false;
+        }
     }
 }
