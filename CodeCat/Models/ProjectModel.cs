263733900f9cc360877c29f67036606ca5eaea28
Eyrun <eyrunh16@ru.is>
2017-05-06 14:26:26 +0000

    Requirements on Project variables added.

--- CodeCat/Models/ProjectModel.cs
+++ CodeCat/Models/ProjectModel.cs
@@ -13,7 +13,8 @@ namespace CodeCat.Models
         public int ID { get; set; }
 
         [Display(Name = "Project name")]
-        [Required(ErrorMessage = "You must enter a name for the project!")]
+        [Required(ErrorMessage = "You must enter a name for the project!"), MaxLength(20, ErrorMessage = "Project name must be less than 20 characters")]
+        //[MaxLength(20, ErrorMessage = "Project name must be less than 20 characters")]
         public string name { get; set; }
 
         //[ForeignKey("User")] //veit ekki hvort þetta sé rétt
