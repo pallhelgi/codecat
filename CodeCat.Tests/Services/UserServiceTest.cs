74a7d57cca3da2641c53d204d1d2a7b74a8e2273
Sigurdur <sigurdur@gmail.com>
2017-05-10 17:33:27 +0000

    +GroundBase for UnitTesting

---
+++ CodeCat.Tests/Services/UserServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCat.Tests.Services
+{
+    public class UserServiceTest
+    {
+        public void SomeTest()
+        {
+            // Arrange:
+
+            // ACT:
+
+            // Assert:
+        }
+    }
+}
