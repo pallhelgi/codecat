d89d42ca144237ce67890fa41ac5b272b6fd70d4
Gudrun Vald <gudrunv16@ru.is>
2017-05-10 18:08:44 +0000

    NoAccessController added and NoAccess data annotations to showProject and showDocument in ProjectController plus FilterConfig.cs

---
+++ CodeCat/Controllers/NoAccessController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NoDirectAccess
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class NoDirectAccessAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.UrlReferrer == null ||
+                        filterContext.HttpContext.Request.Url.Host != filterContext.HttpContext.Request.UrlReferrer.Host)
+            {
+                filterContext.Result = new RedirectToRouteResult(new
+                               RouteValueDictionary(new { controller = "Home", action = "Index", area = "" }));
+            }
+        }
+    }
+}
