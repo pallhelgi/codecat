8e7ec97dc775f9ee5bbc6b954a10ff1595a52faf
Sigurdur <sigurdur@gmail.com>
2017-05-11 11:56:26 +0000

    mergeing

---
+++ CodeCat.Tests/Views/ViewTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCat.Tests.Views
+{
+    public class ViewTest
+    {
+        public void ViewTest();
+        {
+            // Arrange:
+
+            // ACT:
+
+            // Assert:
+        }
+    }
+}
