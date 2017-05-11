01c2621236b5850e5209180074b262d8a2ba7b46
eydisar <eydisar@gmail.com>
2017-05-11 19:02:35 +0000

    unit testing project and user service almost done

--- CodeCat.Tests/Services/UserServiceTest.cs
+++ CodeCat.Tests/Services/UserServiceTest.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CodeCat.Services;
+using CodeCat.Tests;
+using CodeCat.Models;
 
 namespace CodeCat.Tests.Services
 {
@@ -17,18 +19,68 @@ namespace CodeCat.Tests.Services
         [TestInitialize]
         public void Initializer()
         {
-            //LAGA, A EKKI AD VERA NULL
-            _service = new UserService(null);
+            var mockDb = new MockDatabase();
+
+            //Users table
+
+            var u1 = new ApplicationUser
+            {
+                Id = "mellon",
+                Email = "mellon@mellon.com"
+
+            };
+            mockDb.Users.Add(u1);
+
+            var u2 = new ApplicationUser
+            {
+                Id = "jon",
+                Email = "jon@jon.com"
+            };
+            mockDb.Users.Add(u2);
+
+            var u3 = new ApplicationUser
+            {
+                Id = "stina",
+                Email = "stina@stina.com"
+
+            };
+            mockDb.Users.Add(u3);
+
+            //UserProject table
+
+            var UP1 = new UserProjectModel
+            {
+                ID = 1,
+                UserID = "mellon",
+                ProjectID = 1
+            };
+            mockDb.UserProjectModel.Add(UP1);
+
+            var UP2 = new UserProjectModel
+            {
+                ID = 2,
+                UserID = "stina",
+                ProjectID = 2
+            };
+            mockDb.UserProjectModel.Add(UP2);
+
+            _service = new UserService(mockDb);
         }
 
         [TestMethod]
         public void shareWithMellon()
         {
             // Arrange:
+            string email = "mellon@mellon.com";
+            int projID = 2;
 
             // ACT:
 
+            var result = _service.share(email, projID);
+
             // Assert:
+
+            Assert.AreEqual(true, result);
         }
     }
 }
